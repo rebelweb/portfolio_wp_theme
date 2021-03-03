cc1fa0c3511c9bf38bf97bd1e4e64cbc479d69cd
Ryan Condron <rebelwebdevelopment@gmail.com>
2021-03-02 22:31:01 -0600

    register the webpack stylesheet

--- functions.php
+++ functions.php
@@ -1,1 +1,10 @@
 <?php
+
+function portfolio_register_styles() {
+    $theme_version = wp_get_theme()->get('Version');
+    $webpack_stylesheet_path = get_template_directory_uri() . '/assets/main.css';
+
+    wp_enqueue_style('portfolio-webpack-styles',  $webpack_stylesheet_path, null, $theme_version);
+}
+
+add_action('wp_enqueue_scripts', 'portfolio_register_styles');
--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <html>
     <?php get_template_part('./template_parts/html_header') ?>
     <body>
+    <div class="container">
         <?php get_template_part('template_parts/blog_header') ?>
         <?php get_template_part('template_parts/blog_footer') ?>
+    </div>
     </body>
 </html>
