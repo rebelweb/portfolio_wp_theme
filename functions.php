2983cbf604760b6dc4f2dad72b742b2c728ec715
Ryan Condron <rebelwebdevelopment@gmail.com>
2021-03-05 23:48:15 -0600

    Add sidebar

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+const SIDEBAR_NAME = 'sidebar-1';
+
 function portfolio_register_styles() {
     $theme_version = wp_get_theme()->get('Version');
     $webpack_stylesheet_path = get_template_directory_uri() . '/assets/main.css';
@@ -11,4 +13,12 @@ add_action('wp_enqueue_scripts', 'portfolio_register_styles');
 
 function portfolio_base_path() {
     return esc_url(home_url('/'));
+}
+
+function portfolio_sidebar_classes() {
+    return is_active_sidebar(SIDEBAR_NAME) ? 'col-sm-12 col-md-4 col-lg-3' : '';
+}
+
+function portfolio_content_classes() {
+    return is_active_sidebar(SIDEBAR_NAME) ? 'col-sm-12 col-md-8 col-lg-9' : 'col-12';
 }
--- index.php
+++ index.php
@@ -4,6 +4,14 @@
     <body>
     <div class="container">
         <?php get_template_part('template_parts/blog_header') ?>
+        <div class="row">
+            <div class="<?php echo portfolio_content_classes() ?>">
+
+            </div>
+            <div class="<?php echo portfolio_sidebar_classes() ?>">
+                <?php dynamic_sidebar(SIDEBAR_NAME) ?>
+            </div>
+        </div>
         <?php get_template_part('template_parts/blog_footer') ?>
     </div>
     </body>
